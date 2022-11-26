bb3f950066834d28934d268bad628f6a214022e7
razuvayev-d <46049860+razuvayev-d@users.noreply.github.com>
2022-11-26 06:18:28 +0300

    Образ базы данных
    Папка с докерфайлом и sql-скриптами для создания БД

---
+++ DB/sql-scripts/CreateTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE students (
+ID CHAR(5),
+first_name varchar(25),
+last_name varchar(25),
+groupe varchar(20),
+PRIMARY KEY (ID)
+);
---
+++ DB/sql-scripts/InsertData.sql
@@ -0,0 +1,8 @@
+INSERT INTO students (ID, first_name, last_name, groupe)
+VALUES ('1', 'Ingmar', 'Bergman', '00001');
+INSERT INTO students (ID, first_name, last_name, groupe)
+VALUES ('2', 'William', 'Pitt', '00001');
+INSERT INTO students (ID, first_name, last_name, groupe)
+VALUES ('3', 'John', 'Lock', '00002');
+INSERT INTO students (ID, first_name, last_name, groupe)
+VALUES ('4', 'Ada', 'Lovelace', '00002');
